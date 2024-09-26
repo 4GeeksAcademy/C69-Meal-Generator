82e17b29c50dd25fa356eb98e2b6c5971eaaa2e1
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-09-26 01:00:13 +0000

    menu table complete

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, DateTime
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -17,3 +19,20 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=DateTime.utcnow)
+    type = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Menu {self.id}: {self.type}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "type": self.type,
+        }
+
