2e75f7ce858e20763761cf059996af569ae46818
JiHee Ahn <jiheeahn1222@gmail.com>
2024-09-26 01:20:10 +0000

    Basic navbar with no logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+		<nav className="navbar navbar-light bg-light d-flex">
+				<div className="logo px-5 col-8">
+					<Link to="/">
+						<p className="navbar-brand mb-0 h1">TOWA</p>
 					</Link>
 				</div>
-			</div>
+				<div className="login ml-3 col-3 px-5">
+					<Link to="/login">Log-In</Link>
+				</div>
+				<div className="signup ml-3 col-1">
+					<Link to="/signup">Sign up</Link>
+				</div>
 		</nav>
 	);
 };
