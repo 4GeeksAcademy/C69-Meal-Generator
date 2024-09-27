9a03f2589413fef28efeefbd209bfd930dbf8c8b
JiHee Ahn <jiheeahn1222@gmail.com>
2024-09-27 22:43:09 +0000

    navbar with logo and color

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css"
+import towaLogo from "../../img/towa-logo.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light d-flex">
-				<div className="logo px-5 col-8">
-					<Link to="/">
-						<p className="navbar-brand mb-0 h1">TOWA</p>
-					</Link>
-				</div>
-				<div className="login ml-3 col-3 px-5">
-					<Link to="/login">Log-In</Link>
-				</div>
-				<div className="signup ml-3 col-1">
-					<Link to="/signup">Sign up</Link>
-				</div>
+		<nav className="navbar navbar-light d-flex">
+			<div className="logo px-3 col-8 d-flex">
+				<Link to="/">
+					<img className="towaLogo" src={towaLogo} />
+				</Link>
+			</div>
+			<div className="login ml-3 col-3 px-5">
+				<Link to="/login">Log-In</Link>
+			</div>
+			<div className="signup ml-3 col-1">
+				<Link to="/signup">Sign up</Link>
+			</div>
 		</nav>
 	);
 };
