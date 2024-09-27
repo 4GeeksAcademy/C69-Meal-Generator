bf2ac2eedec234337067ff4771d8d9695dc55f3b
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-09-27 23:09:05 +0000

    models are continuing to work

--- src/api/models.py
+++ src/api/models.py
@@ -22,12 +22,15 @@ class User(db.Model):
     
 
 class Menu(db.Model):
+    __tablename__ = "menu"
+
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=func.now())
     type = db.Column(db.String(120), unique=True, nullable=False)
-
+    availability = db.relationship('MenuAvailability')
+    
     def __repr__(self):
-        return f'<Menu {self.id}: {self.type}>'
+        return f'<Menu {self.type}:>'
     
     def serialize(self):
         return {
@@ -36,3 +39,29 @@ class Menu(db.Model):
             "type": self.type,
         }
 
+class MenuAvailability(db.Model):
+    __tablename__ = "menu_availability"
+
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(120), unique=True, nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+    menu_id = db.Column(db.Integer,db.ForeignKey('menu.id'))
+
+    def __repr__(self):
+        return f'<MenuAvailability {self.day}: {self.start_time} to {self.end_time}>'
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "day": self.day,
+            "start_time": self.start_time,
+            "end_time": self.end_time,
+            "menu_id": self.menu_id,
+        }
+    
+    # class Dish(db.Model):
+    #     __tablename__ = "dish"
+
+    #     id = db.Column(db.Integer, primary_key=True)
