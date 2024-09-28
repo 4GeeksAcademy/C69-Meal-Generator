3b95b91cb6c3dfc43f854918bd2aea9109bbeccc
JiHee Ahn <jiheeahn1222@gmail.com>
2024-09-28 00:09:55 +0000

    homepage scheleton

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,28 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
+		<div className="overallWebsite text-center">
+			<p class="toggle-menu-buttons d-inline-flex gap">
+				<a href="#"
+					class="brunch-menu-btn btn btn-outline-light"
+					role="button"
+					id="brunchButton"
+					onclick="toggleFill()">Brunch Menu</a>
+				<a href="#"
+					class="dinner-menu-btn btn btn-outline-light"
+					role="button"
+					id="dinnerButton"
+					onclick="toggleFill()">Dinner Menu</a>
 			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<div className="menu text-center">
+				<h1 className="placeholder-for-menu">MENU</h1>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+
 		</div>
 	);
 };
