468ed3f6e72d0b43afd2d87fff2449ab726990fb
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-09-28 00:50:52 +0000

    finished models

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,9 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    ingredident_restrictions = db.relationship('Ingredient', secondary="restriction", back_populates="restricted_by_users")
+    ingredient_preferences = db.relationship('Ingredient', secondary="preference", back_populates="preferred_by_users")
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -45,7 +48,7 @@ class MenuAvailability(db.Model):
     day = db.Column(db.String(120), unique=True, nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=False)
-    menu_id = db.Column(db.Integer,db.ForeignKey('menu.id'))
+    menu_id = db.Column(db.Integer,db.ForeignKey('menu.id'), nullable=False)
 
     def __repr__(self):
         return f'<MenuAvailability {self.day}: {self.start_time} to {self.end_time}>'
@@ -64,12 +67,13 @@ class MenuAvailability(db.Model):
         __tablename__ = "dish"
 
         id = db.Column(db.Integer, primary_key=True)
-        menu_id = db.Column(db.Integer,db.ForeignKey('menu.id'))
+        menu_id = db.Column(db.Integer,db.ForeignKey('menu.id'), nullable=False)
         name = db.Column(db.String(120), unique=True, nullable=False)
         created_at = db.Column(db.DateTime, nullable=False, default=func.now())
+        ingredients = db.relationship('Ingredient', secondary="dish_ingredient", back_populates="dishes",)
 
         def __repr__(self):
-            return f'<Dish {self.type}:>'
+            return f'<Dish {self.name}:>'
         
         def serialize(self):
             return {
@@ -85,9 +89,13 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     calories = db.Column(db.Float(precision=2),unique=True, nullable=False)
+    dishes = db.relationship('Dish', secondary="dish_ingredient", back_populates="ingredients")
+    restricted_by_users = db.relationship('User', secondary='restriction', back_populates="ingredident_restrictions")
+    preferred_by_users = db.relationship('User', secondary="preference", back_populates="ingredient_preferences")
+    # add protein
 
     def __repr__(self):
-            return f'<Ingredient {self.type}:>'
+            return f'<Ingredient {self.name}:>'
     
     def serialize(self):
             return {
@@ -101,15 +109,31 @@ class DishIngredient(db.Model):
     __tablename__ = "dish_ingredient"
 
     id = db.Column(db.Integer, primary_key=True)
-    dish_id = db.Column(db.Integer,db.ForeignKey('dish.id'))
-    ingredient_id = db.Column(db.Integer,db.ForeignKey('ingredient.id'))
+    dish_id = db.Column(db.Integer, db.ForeignKey('dish.id'), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
 
     def __repr__(self):
-            return f'<DishIngredient {self.type}:>'
+            return f'<DishIngredient {self.id}:>'
+
+class Restriction(db.Model):
+     __tablename__ = "restriction"
+
+     id = db.Column(db.Integer, primary_key=True)
+     user_id =db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+
+     def __repr__(self):
+            return f'<Restriction {self.id}:>'
+     
+
+class Preference(db.Model):
+     __tablename__ = "preference"
+
+     id = db.Column(db.Integer, primary_key=True)
+     user_id =db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+
+     def __repr__(self):
+            return f'<Preferences {self.id}:>'
     
-    def serialize(self):
-            return {
-            "id": self.id,
-            "dish_id": self.dish_id,
-            "ingredient_id": self.ingredient_id,
-        }
+
