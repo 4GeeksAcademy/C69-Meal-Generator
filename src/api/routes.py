9f19a62237416a7db6bd35acd9d8830c3182ea8f
JiHee Ahn <jiheeahn1222@gmail.com>
2024-09-29 19:58:50 +0000

    started methods

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +15,40 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/sign-up', methods=['POST'])
+def handle_sign_up():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if User.query.filter_by(email = email).first():
+        return jsonify({'msg': 'User already exists'}), 400
+    
+    hashed_password = generate_password_hash(password)
+    new_user = User(email = email, password = hashed_password, is_active = True)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    db.session.add(new_user)
+    db.session.commit()
 
-    return jsonify(response_body), 200
+    return jsonify({'msg':'User created successfully'}), 201
+
+@api.route('/login', methods=['POST'])
+def handle_log_in():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    user = User.query.filter_by(email = email).first()
+    if user is None or not check_password_hash(user.password, password):
+        return jsonify({'msg': 'Invalid username or password'}), 401
+    
+    expiration = datetime.time.delta(days = 7)
+    access_token = create_access_token(identity = user.email, expires_delta = expiration)
+    return jsonify({'Token': access_token}), 200
+
+@api.route('private', methods = ['GET'])
+@jwt_required()
+def private_route():
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email = current_user).first()
+    if not user:
+        return jsonify({'msg': 'User not found'}), 404
+    
+    return jsonify(logged_in_as = current_user), 200
+    
