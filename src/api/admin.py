a1be5652656ab6b2bee281b6616f0427c676fca0
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-10-03 01:15:43 +0000

    working routes and end points

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,6 +4,11 @@ from flask_admin import Admin
 from .models import db, User, Menu, Dish, MenuAvailability, Ingredient
 from flask_admin.contrib.sqla import ModelView
 
+class ChildView(ModelView):
+    column_display_pk = True # optional, but I like to see the IDs in the list
+    column_hide_backrefs = False
+    column_list = ('id', 'name', 'parent')
+
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
@@ -11,11 +16,11 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Menu, db.session))
-    admin.add_view(ModelView(Dish, db.session))
-    admin.add_view(ModelView(Ingredient, db.session))
-    admin.add_view(ModelView(MenuAvailability, db.session))
+    admin.add_view(ChildView(User, db.session))
+    admin.add_view(ChildView(Menu, db.session))
+    admin.add_view(ChildView(Dish, db.session))
+    admin.add_view(ChildView(Ingredient, db.session))
+    admin.add_view(ChildView(MenuAvailability, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
