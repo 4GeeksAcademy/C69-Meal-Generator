a92c66b3102b68c6eacf996f3877ca1d58180195
JiHee Ahn <jiheeahn1222@gmail.com>
2024-10-18 14:40:33 +0000

    Made sure the footer stays at the bottom of the page regardless of the resolution

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -23,7 +23,7 @@ const Layout = () => {
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
-        <div>
+        <div className="wrapper">
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
